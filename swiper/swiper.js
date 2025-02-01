dc788f1e6273695e5d2efb5be9a858cf582ec75f
Hellflowers <71571584+Hellflowers@users.noreply.github.com>
2025-02-02 02:18:12 +0300

    Add files via upload

---
+++ swiper/swiper.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+      },
+   speed: 20000,
+   spaceBetween:0,
+   freeMode: true,
+  });
